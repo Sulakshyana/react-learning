b066eca6dcab3f47d2ec40cd6ed6ef9c63a4c8cd
swastika <swastikaghimire100@gmail.com>
2025-04-10 21:20:30 +0545

    added counter limits

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,44 +1,47 @@
 import { useState } from "react";
- function Counter (){
-     const [count, setcount]=useState(0);
-     const MinCount = -10;
-     const MaxCount = 10
-     function  addcount(){
-         setcount(prevcount=>{
-            if (prevcount<MaxCount){
-           return prevcount+1;
-     }else{
+import { FaMinus, FaPlus } from "react-icons/fa6";
+
+function Counter() {
+  const [count, setcount] = useState(0);
+  const MinCount = -10;
+  const MaxCount = 10;
+  function addcount() {
+    setcount((prevcount) => {
+      if (prevcount < MaxCount) {
+        return prevcount + 1;
+      } else {
+        window.alert("you cannot add more than 10 todos");
         return prevcount;
-        window.alert("you cannot add more than 10 todos")
-     }
-    }
-    );
-     }
-     function subtractcount(){
-         setcount(prevcount=>{
-            if (prevcount > MinCount){
-                return prevcount-1;
-            }else{
-            window.alert("Sorry! You cannot go below -10 todos");
-            return prevcount;
-            }
-        }
-            
-        );
-    }
-     return(
-         <div>
-         <div className="row justify-content-end">
-         <div className="col-4 d-flex justify-content-end mx-2">
-         <h3 className="justify-content-center">Completed Task {count}</h3>
-         </div>
-         <div className="col-4 d-flex justify-content-end mx-2">
-             <button className="me-2"  onClick={addcount}><i className="bi bi-plus-lg fs-2"></i></button>
-             <button onClick={subtractcount}><i className="bi bi-dash-lg fs-2"></i></button>
-             </div>
-         </div>
-         </div>
-     );
- }
- 
- export default Counter;
+      }
+    });
+  }
+  function subtractcount() {
+    setcount((prevcount) => {
+      if (prevcount > MinCount) {
+        return prevcount - 1;
+      } else {
+        window.alert("Sorry! You cannot go below -10 todos");
+        return prevcount;
+      }
+    });
+  }
+  return (
+    <div>
+      <div className="row justify-content-end">
+        <div className="col-4 d-flex justify-content-end mx-2">
+          <h3 className="justify-content-center">Completed Task {count}</h3>
+        </div>
+        <div className="col-4 d-flex justify-content-end mx-2">
+          <button className="me-2" onClick={addcount}>
+            <FaPlus />
+          </button>
+          <button onClick={subtractcount}>
+            <FaMinus />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,15 @@
-import './index.css'
-import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home';
-import Counter from './components/Counter';
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Counter from "./Components/Counter";
 
-  const Router: React.FC = () => {
-    return(
+const Router: React.FC = () => {
+  return (
     <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/count' element={ <Counter/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/count" element={<Counter />} />
     </Routes>
-);
-  };
+  );
+};
 
 export default Router;
